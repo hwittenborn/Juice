dd43455c8187595b187184d191e47d48cb02f1e1
Hunter Wittenborn <git@hunterwittenborn.me>
2021-01-28 20:43:55 -0600

    Update build.sh
    Modify build script to create separate packages for stable and beta releases

--- build.sh
+++ build.sh
@@ -2,23 +2,40 @@
 PKG="Juice"
 
 ## SOME OTHER VARIABLES(DON'T TOUCH PLEASE) ##
-VERSION=$(grep "Version:" Build/DEBIAN/control | awk '{print $2}')
+VERSION_STABLE=$(grep "Version:" Build/DEBIAN/control-stable | awk '{print $2}')
+VERSION_BETA=$(grep "Version:" Build/DEBIAN/control-beta | awk '{print $2}')
 
 ##################
 ## BEGIN SCRIPT ##
 ##################
 
-echo "[] Please input a version number. The current version is "$VERSION"."
+echo "=== "$PKG" Package Builder ==="
+echo "[] Preparing..."
+  rm *.deb &> /dev/null
+echo "[] Please input a version number. The current versions are '"$VERSION_STABLE"' and '"$VERSION_BETA"'."
   read -p ">> " NEW_VERSION
 
 echo "[] Setting new version number..."
-  CONTROL=$(sed "s\Version.*\Version: `echo $NEW_VERSION`\g" Build/DEBIAN/control)
-  echo "$CONTROL" > Build/DEBIAN/control
+echo "$NEW_VERSION" | grep -q beta
+  if [ $? != 0 ]; then
+    CONTROL_FILE="control-stable"
+    CONTROL=$(sed "s\Version.*\Version: `echo $NEW_VERSION`\g" Build/DEBIAN/"$CONTROL_FILE")
+    echo "$CONTROL" > Build/DEBIAN/"$CONTROL_FILE"
+    cp Build/DEBIAN/"$CONTROL_FILE" Build/DEBIAN/control
+  else
+    CONTROL_FILE="control-beta"
+    CONTROL=$(sed "s\Version.*\Version: `echo $NEW_VERSION`\g" Build/DEBIAN/"$CONTROL_FILE")
+    echo "$CONTROL" > Build/DEBIAN/"$CONTROL_FILE"
+    cp Build/DEBIAN/"$CONTROL_FILE" Build/DEBIAN/control
+  fi
   
-echo "[] Building "$PKG"..."
+echo "[] Building "$PKG"-"$NEW_VERSION"..."
   dpkg -b Build
 
-echo "Moving Build.deb to "$PKG"-"$NEW_VERSION".deb"
+echo "[] Moving Build.deb to "$PKG"-"$NEW_VERSION".deb"
   mv Build.deb "$PKG"-"$NEW_VERSION".deb
-  
+ 
+echo "[] Cleaning up..."
+rm Build/DEBIAN/control
+
 echo "[] DONE"
